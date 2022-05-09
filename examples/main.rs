10701b82edc93bed241069812f41046bc244903e
Cameron Elliott <868689+cameronelliott@users.noreply.github.com>
2022-05-08 19:05:42 -0700

    add macos example

--- examples/main.rs
+++ examples/main.rs
@@ -13,10 +13,31 @@ fn bind_process() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+#[cfg(not(target_os = "macos"))]
 pub fn main() -> Result<(), Box<dyn Error>> {
     println!("Total cores : {}", get_core_num());
 
     let cores = (0..get_core_num()).step_by(2).collect::<Vec<usize>>();
+    println!("Binding thread to cores : {:?}", &cores);
+    set_thread_affinity(&cores)?;
+
+    let bound_cores = get_thread_affinity()?;
+    println!("\tCurrent thread affinity : {:?}", bound_cores);
+    println!("\tTotal cores : {}", get_core_num());
+
+    assert_eq!(bound_cores, cores.as_slice());
+
+    #[cfg(target_os = "windows")]
+    bind_process()?;
+
+    Ok(())
+}
+
+#[cfg(target_os = "macos")]
+pub fn main() -> Result<(), Box<dyn Error>> {
+    println!("Total cores : {}", get_core_num());
+
+    let cores = vec![42]; //42 is an affinity tag, see the readme
     println!("Binding thread to cores : {:?}", &cores);
     set_thread_affinity(&cores)?;
 
